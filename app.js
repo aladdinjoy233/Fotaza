45ea0c9fececb48f83746eb75aab3be36c69155b
Aladdin <ninjalover249@gmail.com>
2022-12-23 11:39:42 -0300

    Import sequelize and create DB folders and files

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var sequelize = require('./database/db');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +23,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// conectar a la db
+sequelize.sync({ force: true })
+	.then(() => console.log('Conectado a la base de datos!'))
+	.catch(err => console.log(err));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
