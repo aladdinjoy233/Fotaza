dfe75587c61b66042ea5379827c8d8f11b47c194
Aladdin <ninjalover249@gmail.com>
2022-12-23 11:45:08 -0300

    Create missing folders and files

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var sequelize = require('./database/db');
 var session = require('express-session');
+
+var sequelize = require('./database/db');
+require('./database/asociations');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
