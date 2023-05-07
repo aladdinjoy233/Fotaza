593d60dac9f879d29b998f160086c068150fcc01
Aladdin <ninjalover249@gmail.com>
2023-05-07 12:43:56 -0300

    Main header nav

--- public/js/global.js
+++ public/js/global.js
@@ -1,3 +1,37 @@
+// Obtener datos sencillos del usuario.
+// Se guardan en el localStorage para evitar hacer multiplos
+// fetch en cada recarga de la pagina
+function getUser() {
+	const cachedUserData = localStorage.getItem('user');
+
+	// Si existe los datos en el cache, devuelve los datos
+	if (cachedUserData) {
+		const user = JSON.parse(cachedUserData);
+		if (user.id == userId) {
+			return user;
+		}
+
+		localStorage.removeItem('user');
+	}
+	
+	// Si no existe en el cache, busca los datos
+	if (!userId) return null;
+	return fetch(`/user/get/${userId}`)
+	.then(res => res.json())
+	.then(data => {
+		try {
+			localStorage.setItem('user', JSON.stringify(data));
+		} catch (err) {
+			console.log('Error con el localStorage: ', err);
+		}
+		return data
+	})
+	.catch(err => {
+		console.log(err)
+		return null
+	});
+}
+
 // Agregar error a un input
 function formError(element, message) {
 	const elem = document.querySelector(element)
